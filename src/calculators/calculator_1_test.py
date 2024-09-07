e1135785f8cb682a05d7fc1f4732faf0d0c73b15
narcelllo <marcello_magno@hotmail.com>
2024-09-07 13:09:22 -0300

    Teste response format

---
+++ src/calculators/calculator_1_test.py
@@ -0,0 +1,19 @@
+from typing import Dict
+from .calculator_1 import Calculator1
+
+class MockRequest:
+    def __init__(self, body: Dict) -> None:
+        self.json = body
+
+def test_calulate():
+    mock_request = MockRequest(body= {"number": 3})
+    calculator1 = Calculator1()
+
+    response = calculator1.calculate(mock_request)
+    print()
+    print(response)
+
+    # Teste response format
+    assert "data" in response
+    assert "Calculator" in response["data"]
+    assert "result" in response["data"]
