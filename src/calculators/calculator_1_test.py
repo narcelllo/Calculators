cb8adf30580fb29d10a78b59aba56d0cfbe45eee
narcelllo <marcello_magno@hotmail.com>
2024-09-17 13:43:52 -0300

    Added aroute calculator 2

--- src/calculators/calculator_1_test.py
+++ src/calculators/calculator_1_test.py
@@ -6,7 +6,7 @@ class MockRequest:
     def __init__(self, body: Dict) -> None:
         self.json = body
 
-def test_calulate():
+def test_calculate():
     mock_request = MockRequest(body= {"number": 3})
     calculator1 = Calculator1()
 
--- src/calculators/calculator_2_test.py
+++ src/calculators/calculator_2_test.py
@@ -1,7 +1,12 @@
-from typing import Dict
+from typing import Dict, List
 from pytest import raises
 from .calculator_2 import Calculator2
 from src.drivers.numpy_handler import NumpyHandler
+from src.drivers.interfaces.driver_handler_interface import DriverHandlerInterface
+
+class MockDriverHandler(DriverHandlerInterface):
+    def standarf_derivation(self, numbers: List[float]) -> float:
+        return 3
 
 class MockRequest:
     def __init__(self, body: Dict) -> None:
@@ -16,4 +21,14 @@ def test_calculate_integration():
     
     assert isinstance(formated_response, dict)
     assert formated_response == {'data': {'Calculator': 2, 'result': 0.08}}
+
+def test_calculate():
+    mock_request = MockRequest({"numbers": [2.12, 4.62, 1.32]})
+
+    driver = MockDriverHandler()
+    calculator_2 = Calculator2(driver)
+    formated_response = calculator_2.calculate(mock_request)
+    
+    assert isinstance(formated_response, dict)
+    assert formated_response == {'data': {'Calculator': 2, 'result': 0.33}}
     
