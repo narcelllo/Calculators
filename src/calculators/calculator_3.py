542aa6d48dcc51d8e79c6e5ff0e31bc6d52729c2
narcelllo <marcello_magno@hotmail.com>
2024-09-19 16:01:28 -0300

    Added calss erros

--- src/calculators/calculator_3.py
+++ src/calculators/calculator_3.py
@@ -21,7 +21,7 @@ class Calculator3:
 
     def __validate_body(self, body: Dict) -> List[float]:
         if "numbers" not in body:
-            raise Exception('body does not contain numbers! - incorrect format! \n Examble format: {"numbers": 3.4, 4, 64.13}')
+            raise Exception('body does not contain numbers! - incorrect format! \n Examble format: {"numbers": [3.4, 4, 64.13]}')
         
         input_data = body["numbers"]
         return input_data
